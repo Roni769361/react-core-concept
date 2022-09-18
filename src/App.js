0689d09239d0a7d688ba3028fcf48a75cd8395f6
Ovimonno Chy Roni <oceanc9361@gmail.com>
2022-09-18 23:44:54 +0600

    that

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var name = "Rani"
-  var years = 150000
+
  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello {name} react Man</h1>
-        <h5>{name} is {years} years Old</h5>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done for React <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur rem facilis vel adipisci eos tenetur in nesciunt ratione aliquid corporis vero, repellat modi eaque, recusandae, culpa dicta ipsa facere ipsum.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>I am React User! for you</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+
       </header>
     </div>
   );
 }
 
+function Person(){
+  return (
+  <div style={{border:"1px solid red", borderRadius:"30px", padding: "10px", margin:"10px"}}>
+    <h1>Name: Roni</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nesciunt.</p>
+  </div>
+  )
+}
+
 export default App;
+
+
