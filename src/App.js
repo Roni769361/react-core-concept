25ef6e456f1d3aa2af52746239ea6bb65e423538
Ovimonno Chy Roni <oceanc9361@gmail.com>
2022-09-19 22:32:27 +0600

    Dainamic

--- src/App.js
+++ src/App.js
@@ -2,31 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const productList=[
+    {name: "Shosma", price: "300Tk"},
+    {name: "Laptop", price: "300000Tk"},
+    {name: "Mobile", price: "150000Tk"},
+    {name: "Charger", price: "320Tk"},
+    {name: "Keyboard", price: "800Tk"},
 
+  ]
  
   return (
     <div className="App">
       <header className="App-header">
-      <h1>I am React User! for you</h1>
-      <Person name="Roni Nath" p="This is Roni's Items."></Person>
-      <Person name="Rudresh is Software Engenner" p="This is Only rudresh's house"></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <h5>Simple Component Create</h5>
+      {
+        productList.map(pro => <Product prudoctHear={pro}></Product>)
+      }
+
+
+        {/* <Product prudoctHear={productList[0]}></Product>
+        <Product prudoctHear={productList[1]}></Product> */}
 
       </header>
     </div>
   );
 }
 
-function Person(props){
-  return (
-  <div style={{border:"1px solid red", borderRadius:"30px", padding: "10px", margin:"10px"}}>
-    <h1>Name: {props.name}</h1>
-    <p>{props.p}</p>
-  </div>
+function Product(props){
+  const stylehear={
+    color:'green',
+    backgroundColor:"#9A9A9A",
+    margin:"15px",
+    padding:"10px",
+    hight:"300px",
+    width: "280px",
+    borderRadius:"10px"
+
+
+  }
+  const {name, price} = props.prudoctHear;
+  console.log(name, price);
+  return(
+    <div style={stylehear}>
+      <h4>{name}</h4>
+      <h2>{price}</h2>
+      <button>Buy Now</button>
+    </div>
   )
 }
+
 
 export default App;
 
