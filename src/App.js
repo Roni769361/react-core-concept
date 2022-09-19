76b30c2247bc454578cf9e2b4375c46fa9ec5296
Ovimonno Chy Roni <oceanc9361@gmail.com>
2022-09-19 09:37:42 +0600

    thart

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>I am React User! for you</h1>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Roni Nath" p="This is Roni's Items."></Person>
+      <Person name="Rudresh is Software Engenner" p="This is Only rudresh's house"></Person>
       <Person></Person>
       <Person></Person>
       <Person></Person>
@@ -19,11 +19,11 @@ function App() {
   );
 }
 
-function Person(){
+function Person(props){
   return (
   <div style={{border:"1px solid red", borderRadius:"30px", padding: "10px", margin:"10px"}}>
-    <h1>Name: Roni</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nesciunt.</p>
+    <h1>Name: {props.name}</h1>
+    <p>{props.p}</p>
   </div>
   )
 }
